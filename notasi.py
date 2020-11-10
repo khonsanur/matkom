401fab413431e3fce196e7281df2c46b185f1e84
khonsanur <73088939+khonsanur@users.noreply.github.com>
2020-11-10 23:59:21 +0700

    Add files via upload

---
+++ notasi.py
@@ -0,0 +1,25 @@
+print("No.1")
+#Notasi Sigma kuadrat dari 13 s.d. 17
+hasil = 0
+n = 17
+for i in range (13, n+1):
+  hasil= hasil + i*5 + 2
+print("hasilnya adalah:", hasil)
+end=()
+print("\n")
+print("No.2")
+# Notasi Sigma kuadrat dari 5 s.d. 10
+hasil = 0
+n = 10
+for i in range (5, n+1):
+  hasil= hasil + i*i + 2*i - 1
+print("hasilnya adalah:", hasil)
+end=()
+print("\n") 
+print("No.3")
+# Notasi PI kuadrat dari 4 s.d. 8
+hasil = 1
+n = 8
+for i in range (4, n+1):
+  hasil = hasil * (i - 3)
+print("hasilnya adalah:", hasil)
